31b4ed717fed398e3471b3e4ad9970d4854331a6
2Nos <97288892+2Nos@users.noreply.github.com>
2022-11-28 17:41:15 +0900

    EchoServer

---
+++ EchoServer/EchoServer/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+class User
+{
+    public Socket userSock;
+    public byte[] sendBuffer;
+    public byte[] receiveBuffer;
+    public User()
+    {
+        sendBuffer = new byte[128];
+        receiveBuffer = new byte[128];
+    }
+    public User(Socket _sock)
+    {
+        userSock = _sock;
+        sendBuffer = new byte[128];
+        receiveBuffer = new byte[128];
+    }
+    // User클래스에서는 어떠한 기능이 필요
+    // 버퍼를 초기화
+    public void ClearSendBuffer()
+    {
+        Array.Clear(sendBuffer, 0, sendBuffer.Length);
+    }
+    public void ClearReceiveBuffer()
+    {
+        Array.Clear(receiveBuffer, 0, receiveBuffer.Length);
+    }
+}
+
